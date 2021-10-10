0ad7fdd13798fe1650c1b2f52aef4d9f672fc3df
Rohit <rrajak7499@gmail.com>
2021-10-10 15:48:05 +0530

    Added Lower Case and Counting letter and word

--- src/Component/MyInputBox.js
+++ src/Component/MyInputBox.js
@@ -6,6 +6,11 @@ export default function MyInputBox(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+
+    const handleLoClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     
     const handleOnChange  = (event) => {
         setText(event.target.value);
@@ -14,11 +19,19 @@ export default function MyInputBox(props) {
     const [text, setText] = useState("Enter text here.");
 
     return (
+        <>
         <div className="mb-3">
             <h1>{props.heading}</h1>
             <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="7"></textarea>
             <button className="btn btn-primary m-3" onClick={handleUpClick} >Make UpperCase</button>
-            <button className="btn btn-primary m-3" >Make LowerCase</button>
+            <button className="btn btn-primary m-3" onClick={handleLoClick} >Make LowerCase</button>
         </div>
+        <div className="container">
+            <h1>Text counting</h1>
+            <p>{text.length} Letters and {text.split(" ").length} Words</p>
+            <h2>Text preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
