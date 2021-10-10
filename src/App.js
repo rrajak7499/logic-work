678c6b299be802bf2049ab8471704926422b1cf3
Rohit <rrajak7499@gmail.com>
2021-10-10 21:41:44 +0530

    Added Dark mode and light mode toogle button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Navbar from './Component/Navbar'
 import MyInputBox from './Component/MyInputBox'
+import React, { useState } from 'react'
+
 
 function App() {
+
+  const [mode, setMode] = useState("white");
+
+  const tooggleMode = () => {
+    if(mode === 'white') {
+      setMode('balck');
+      document.body.style.color= "white";
+      document.body.style.backgroundColor = "#303132";
+    } else {
+      setMode('white');
+      document.body.style.color= "black";
+      document.body.style.backgroundColor = "#f8f9fa";
+    }
+  }
+
   return (
     <>
-      <Navbar title="Jio" home="Home" about="About Us"/>
+      <Navbar title="Jio" home="Home" about="About Us" mode={mode} tooggleMode={tooggleMode}/>
       <div className="container">
       <MyInputBox heading="Input text below:" />
       </div>
--- src/Component/MyInputBox.js
+++ src/Component/MyInputBox.js
@@ -2,16 +2,31 @@ import React, {useState} from 'react'
 
 export default function MyInputBox(props) {
     
+    // Making upper case 
     const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
     }
 
+    // Making Lower case 
     const handleLoClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
     }
+
+    // Copying Text 
+    const handleCopy = () => {
+        let text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
     
+    // Remove Extra spaces 
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+
     const handleOnChange  = (event) => {
         setText(event.target.value);
     }
@@ -22,12 +37,16 @@ export default function MyInputBox(props) {
         <>
         <div className="mb-3">
             <h1>{props.heading}</h1>
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="7"></textarea>
+            <textarea id="myInput" className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="7"></textarea>
             <button className="btn btn-primary m-3" onClick={handleUpClick} >Make UpperCase</button>
             <button className="btn btn-primary m-3" onClick={handleLoClick} >Make LowerCase</button>
+            <button className="btn btn-primary m-3" onClick={handleCopy} >Copy Text</button>
+            <button className="btn btn-primary m-3" onClick={handleExtraSpaces} >Remove Extra Spaces</button>
+
+            {/* <button className="btn btn-primary m-3" onClick={handleCopyClick} >Copy Clipboard</button> */}
         </div>
         <div className="container">
-            <h1>Text counting</h1>
+            <h1 className={`text-${props.mode === 'white' ? 'black' : 'white'}`}>Text counting</h1>
             <p>{text.length} Letters and {text.split(" ").length} Words</p>
             <h2>Text preview</h2>
             <p>{text}</p>
